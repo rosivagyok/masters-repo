4228bd7dc2a73fde50a4ec380e16b1dc8cab2e84
roh <rosivagyok@gmail.com>
2018-04-12 14:46:04 +0200

    distances angles

--- feature_smooth.py
+++ feature_smooth.py
@@ -3,7 +3,7 @@ from scipy.interpolate import interp1d
 from itertools import chain
 
 def feature_smooth(pose_feats):
-    for j in chain(range(0, 10, 2), range(26, len(pose_feats), 2)):
+    for j in chain(range(0, 12, 2), range(26, 54, 2)):
 
         # Create id array for 0 element ranges
         idx = np.flatnonzero(pose_feats[:,j] == 0)
@@ -18,7 +18,7 @@ def feature_smooth(pose_feats):
         for i in range(0, len(idx)):
             # Last iteration change
             if i == len(idx)-1:
-                mayval2 = idx[i]
+                myval2 = idx[i]
                 range_enum[k,0:2] = np.array([myval1, myval2])
             elif np.logical_and(idx[i+1] - idx[i] == 1, myval1 == 0): 
                 myval1 = idx[i]
@@ -46,21 +46,27 @@ def feature_smooth(pose_feats):
         # Iterate through the zero ranges, interpolate from beginning [x,y] coordinates
         # and sample data from the created interpolation points.
         for l in range(0, len(range_enum)):
-            pathXY = np.concatenate(([pose_feats[range_enum[l, 0].astype(int) - 1, j:j+2]], [pose_feats[range_enum[l, 1].astype(int) + 1, j:j+2]]),0)
-            stepLengths = np.sqrt(np.sum(np.array([(pathXY[1,0] - pathXY[0,0]), (pathXY[1,1] - pathXY[0,1])])**2, 0))
-            stepLengths = np.array([0,stepLengths])
-            cumulativeLen = np.cumsum(stepLengths)
-            finalStepLocs = np.linspace(0, cumulativeLen[-1], 100)
-            finalPathXY = interp1d(cumulativeLen,pathXY,axis=0)(finalStepLocs)
+            if range_enum[l, 1].astype(int) + 1 == len(pose_feats):
+                pass
+            else:
+                if range_enum[l, 1].astype(int) + 1 != len(pose_feats)-1:
+                    pathXY = np.concatenate(([pose_feats[range_enum[l, 0].astype(int) - 1, j:j+2]], [pose_feats[range_enum[l, 1].astype(int) + 1, j:j+2]]),0)
+                else:
+                    pathXY = np.concatenate(([pose_feats[range_enum[l, 0].astype(int) - 1, j:j+2]], [pose_feats[range_enum[l, 1].astype(int), j:j+2]]),0)
+                stepLengths = np.sqrt(np.sum(np.array([(pathXY[1,0] - pathXY[0,0]), (pathXY[1,1] - pathXY[0,1])])**2, 0))
+                stepLengths = np.array([0,stepLengths])
+                cumulativeLen = np.cumsum(stepLengths)
+                finalStepLocs = np.linspace(0, cumulativeLen[-1], 100)
+                finalPathXY = interp1d(cumulativeLen,pathXY,axis=0)(finalStepLocs)
 
-            # Pick points
-            a = np.arange(range_enum[l,0],range_enum[l,1],1)
-            b = len(finalPathXY)/len(a)-1;
-            c = finalPathXY[np.arange(b,len(a)*b,b),0:2]
+                # Pick points
+                a = np.arange(range_enum[l,0],range_enum[l,1]+1,1)
+                b = len(finalPathXY)/len(a)-1;
+                c = finalPathXY[np.linspace(0,len(finalPathXY)-1,len(a)).astype(int),0:2]
 
-            # Swap data
-            pose_feats[a,j:j+1] = c
+                # Swap data
+                pose_feats[a.astype(int),j:j+2] = c
 
-        pose_feats_smooth = pose_feats
+    pose_feats_smooth = pose_feats
 
     return pose_feats_smooth
--- json_parser_train.py
+++ json_parser_train.py
@@ -8,9 +8,9 @@ from feature_smooth import feature_smooth
 #Load 
 path = "E:\\MATLAB\\Project\\Project\\keypoints_PAN\\"
 json_files = os.listdir(path)
-face_feats_all = np.empty([2, len(json_files), 210], dtype=np.float64)
-pose_feats_all = np.empty([2, len(json_files), 54], dtype=np.float64)
-pose_feats = np.empty([len(json_files), 66], dtype=np.float64)
+face_feats_all = np.zeros([2, len(json_files), 210], dtype=np.float64)
+pose_feats_all = np.zeros([2, len(json_files), 54], dtype=np.float64)
+pose_feats = np.zeros([len(json_files), 66], dtype=np.float64)
 
 for idx in range(0,len(json_files)):
     data = json.load(open(path + json_files[idx]))
@@ -26,26 +26,26 @@ for idx in range(0,len(json_files)):
             pass
 
     else:
-        face_feats_all[0,idx] = np.empty([210])
-        face_feats_all[1,idx] = np.empty([210])
-        pose_feats_all[0,idx] = np.empty([54])
-        pose_feats_all[1,idx] = np.empty([54])
+        face_feats_all[0,idx] = np.zeros([210])
+        face_feats_all[1,idx] = np.zeros([210])
+        pose_feats_all[0,idx] = np.zeros([54])
+        pose_feats_all[1,idx] = np.zeros([54])
     
     # Similarity check for false positive detections;
     # check which candidate yields more keypoints, use the one that has
     # more
-    k = np.count_nonzero([pose_feats_all[0,idx,0:2], pose_feats_all[0,idx,2:4], pose_feats_all[0,idx,42:44], pose_feats_all[0,idx,45:47], pose_feats_all[0,idx,6:8], pose_feats_all[0,idx,15:17]])
-    a = np.count_nonzero([pose_feats_all[1,idx,0:2], pose_feats_all[1,idx,2:4], pose_feats_all[1,idx,42:44], pose_feats_all[1,idx,45:47], pose_feats_all[1,idx,6:8], pose_feats_all[1,idx,15:17]])
+    k = np.count_nonzero([pose_feats_all[0,idx,0:2], pose_feats_all[0,idx,3:5], pose_feats_all[0,idx,42:44], pose_feats_all[0,idx,45:47], pose_feats_all[0,idx,6:8], pose_feats_all[0,idx,15:17]])
+    a = np.count_nonzero([pose_feats_all[1,idx,0:2], pose_feats_all[1,idx,3:5], pose_feats_all[1,idx,42:44], pose_feats_all[1,idx,45:47], pose_feats_all[1,idx,6:8], pose_feats_all[1,idx,15:17]])
 
     if k < a:
-        pose_feats_all[0,idx] = pose_feats_all[1,idx]
-        face_feats_all[0,idx] = face_feats_all[1,idx]
+        pose_feats_all[0,idx,:] = pose_feats_all[1,idx,:]
+        face_feats_all[0,idx,:] = face_feats_all[1,idx,:]
     else:
         pass
 
     # Nose - Neck
     pose_feats[idx,0:2] = np.array([pose_feats_all[0,idx,0:2]])
-    pose_feats[idx,2:4] = np.array([pose_feats_all[0,idx,2:4]])
+    pose_feats[idx,2:4] = np.array([pose_feats_all[0,idx,3:5]])
 
     # REye - LEye
     pose_feats[idx,4:6] = np.array([pose_feats_all[0,idx,42:44]])
@@ -56,12 +56,12 @@ for idx in range(0,len(json_files)):
     pose_feats[idx,10:12] = np.array([pose_feats_all[0,idx,15:17]])
 
     # REye_refined
-    pose_feats[idx][26:40] = np.ndarray.flatten(np.array([face_feats_all[0,idx,204:206], face_feats_all[0,idx,108:110], face_feats_all[0,idx,111:113],
+    pose_feats[idx,26:40] = np.ndarray.flatten(np.array([face_feats_all[0,idx,204:206], face_feats_all[0,idx,108:110], face_feats_all[0,idx,111:113],
                                        face_feats_all[0,idx,114:116], face_feats_all[0,idx,117:119], face_feats_all[0,idx,120:122], 
                                        face_feats_all[0,idx,123:125]]))
 
     # LEye_refined
-    pose_feats[idx][40:54] = np.ndarray.flatten(np.array([face_feats_all[0,idx,207:209], face_feats_all[0,idx,126:128], face_feats_all[0,idx,129:131],
+    pose_feats[idx,40:54] = np.ndarray.flatten(np.array([face_feats_all[0,idx,207:209], face_feats_all[0,idx,126:128], face_feats_all[0,idx,129:131],
                                        face_feats_all[0,idx,132:134], face_feats_all[0,idx,135:137], face_feats_all[0,idx,138:140], 
                                        face_feats_all[0,idx,141:143]]))
 
@@ -79,5 +79,44 @@ for idx in range(0,len(json_files)):
 pose_feats_smooth = feature_smooth(pose_feats)
 
 # Calculate the rest of the feature space (distances, angles)
-for i in range(0, len(pose_feats)):
+for i in range(0, len(pose_feats_smooth)):
+
     # Recalculate coordinates to nose origin
+    pose_feats_smooth[i,2:4] = pose_feats_smooth[i,2:4] - pose_feats_smooth[i,0:2]
+    pose_feats_smooth[i,4:6] = pose_feats_smooth[i,4:6] - pose_feats_smooth[i,0:2]
+    pose_feats_smooth[i,6:8] = pose_feats_smooth[i,6:8] - pose_feats_smooth[i,0:2]
+    pose_feats_smooth[i,8:10] = pose_feats_smooth[i,8:10] - pose_feats_smooth[i,0:2]
+    pose_feats_smooth[i,10:12] = pose_feats_smooth[i,10:12] - pose_feats_smooth[i,0:2]
+    pose_feats_smooth[i,26:40] = np.subtract(pose_feats_smooth[i,26:40].reshape((7,2)), pose_feats_smooth[i,0:2]).reshape((1,14))
+    pose_feats_smooth[i,40:54] = np.subtract(pose_feats_smooth[i,40:54].reshape((7,2)), pose_feats_smooth[i,0:2]).reshape((1,14))
+
+    pose_feats_smooth[i,0:2] = [0, 0]
+
+    # Euclidean distance between all face features.
+    pose_feats_smooth[i,12] = np.linalg.norm(pose_feats_smooth[i,0:2] - pose_feats_smooth[i,4:6])
+    pose_feats_smooth[i,13] = np.linalg.norm(pose_feats_smooth[i,0:2] - pose_feats_smooth[i,6:8])
+    pose_feats_smooth[i,14] = np.linalg.norm(pose_feats_smooth[i,4:6] - pose_feats_smooth[i,6:8])
+
+    # Euclidean distance between neck and all face features.
+    pose_feats_smooth[i,15] = np.linalg.norm(pose_feats_smooth[i,2:4] - pose_feats_smooth[i,0:2])
+    pose_feats_smooth[i,16] = np.linalg.norm(pose_feats_smooth[i,2:4] - pose_feats_smooth[i,4:6])
+    pose_feats_smooth[i,17] = np.linalg.norm(pose_feats_smooth[i,2:4] - pose_feats_smooth[i,6:8])
+
+    # Euclidean distance between RShoulder and all face features.
+    pose_feats_smooth[i,18] = np.linalg.norm(pose_feats_smooth[i,8:10] - pose_feats_smooth[i,0:2])
+    pose_feats_smooth[i,19] = np.linalg.norm(pose_feats_smooth[i,8:10] - pose_feats_smooth[i,4:6])
+    pose_feats_smooth[i,20] = np.linalg.norm(pose_feats_smooth[i,8:10] - pose_feats_smooth[i,6:8])
+
+    # Euclidean distance between LShoulder and all face features.
+    pose_feats_smooth[i,21] = np.linalg.norm(pose_feats_smooth[i,10:12] - pose_feats_smooth[i,0:2])
+    pose_feats_smooth[i,22] = np.linalg.norm(pose_feats_smooth[i,10:12] - pose_feats_smooth[i,4:6])
+    pose_feats_smooth[i,23] = np.linalg.norm(pose_feats_smooth[i,10:12] - pose_feats_smooth[i,6:8])
+
+    # Angle between vec(neck,nose) and vec(neck,LShoulder)
+    #u = np.cross([(pose_feats[i,2] - pose_feats[i,0]), ((pose_feats[i,3] - pose_feats[i,1])),0],[(pose_feats[i,2] - pose_feats[i,10]), ((pose_feats[i,3] - pose_feats[i,11])),0])
+
+    #v = np.dot([(pose_feats[i,2] - pose_feats[i,0]), ((pose_feats[i,3] - pose_feats[i,1])),0],[(pose_feats[i,2] - pose_feats[i,10]), ((pose_feats[i,3] - pose_feats[i,11])),0])
+
+    pose_feats_smooth[i,24] = np.arccos(np.clip(np.dot(np.linalg.norm(pose_feats_smooth[i,0:2] - pose_feats_smooth[i,2:4]),np.linalg.norm(pose_feats_smooth[i,8:10] - pose_feats_smooth[i,2:4])),-1.0,1.0))
+
+    #pose_feats_smooth[i,25] = 
