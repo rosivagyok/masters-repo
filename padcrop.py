c784fdea2b03e8c4889cdbf4bbc8a8869b697212
roh <rosivagyok@gmail.com>
2018-05-15 20:10:41 +0200

    try me

--- padcrop.py
+++ padcrop.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy as sp
 import os
 import scipy.ndimage, scipy.misc
+import cv2
 """ Crop and pad depth images to 1920x1080 """
 
 # output folder
@@ -14,9 +15,12 @@ imagelist = os.listdir(path)
 
 for i in range(0, np.size(imagelist,0)-1):
     frame = scipy.ndimage.imread(path + imagelist[i]) # load image
+    #frame = cv2.cvtColor( frame, cv2.COLOR_RGB2GRAY )
 
-    frame = scipy.misc.imresize(frame, 3) # resize image by 3.0 factor
-    frame = np.delete(frame, [frame[0:95,:], frame[1080:frame.shape[0]-1,:]], axis=0) # crop top and bottom rows
-    pad = np.zeros(1080,192)
-    frame = np.concatenate((pad,frame,pad),axis=1) # pad left and right
+    frame = scipy.misc.imresize(frame, 300) # resize image by 3.0 factor
+    frame = np.delete(frame, range(0,96), axis=0) # crop top and bottom rows , frame[1080:frame.shape[0]-1,:]], 
+    frame = np.delete(frame, range(1080,frame.shape[0]), axis=0)
+    pad = np.zeros([1080,192])
+    frame = np.concatenate((pad,frame),axis=1) # pad left and right
+    frame = np.concatenate((frame,pad),axis=1) # pad left and right
     scipy.misc.imsave(outpath + imagelist[i],frame) # save image to output folder 
